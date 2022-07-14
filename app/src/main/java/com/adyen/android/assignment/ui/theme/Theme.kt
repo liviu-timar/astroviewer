9f63da4269f5eff2970daf976bdcd50d480d2679
Liviu Timar <liviu.timar@autoliv.com>
2022-07-14 16:40:53 +0300

    Resource and theme changes

--- app/src/main/java/com/adyen/android/assignment/ui/theme/Theme.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/theme/Theme.kt
@@ -16,7 +16,7 @@ private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200,
-    background = Background
+    background = BackgroundColor
 
     /* Other default colors to override
     background = Color.White,
