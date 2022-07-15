e079f14e2052ff65fe0f23e6cf0fdd2c7a365438
Liviu Timar <liviu.timar@autoliv.com>
2022-07-15 17:00:34 +0300

    Disabled ripple effect throughout the app

--- app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.ripple.LocalRippleTheme
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import com.adyen.android.assignment.ui.picturelist.AstronomyPictureListScreen
 import com.adyen.android.assignment.ui.picturelist.AstronomyPictureListViewModel
 import com.adyen.android.assignment.ui.theme.MainTheme
+import com.adyen.android.assignment.ui.theme.NoRippleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -26,7 +29,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    AstronomyPictureListScreen(viewModel = viewModel)
+                    // Use CompositionLocal to disable the ripple effect down the Composition tree
+                    CompositionLocalProvider(LocalRippleTheme provides NoRippleTheme) {
+                        AstronomyPictureListScreen(viewModel = viewModel)
+                    }
                 }
             }
         }
--- app/src/main/java/com/adyen/android/assignment/ui/theme/Theme.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/theme/Theme.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
+import androidx.compose.material.ripple.RippleAlpha
+import androidx.compose.material.ripple.RippleTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -27,6 +30,19 @@ private val LightColorPalette = lightColors(
     onSurface = Color.Black,
     */
 )
+
+object NoRippleTheme : RippleTheme {
+    @Composable
+    override fun defaultColor(): Color = Color.Transparent
+
+    @Composable
+    override fun rippleAlpha() = RippleAlpha(
+        draggedAlpha = 0.0f,
+        focusedAlpha = 0.0f,
+        hoveredAlpha = 0.0f,
+        pressedAlpha = 0.0f,
+    )
+}
 
 @Composable
 fun MainTheme(
