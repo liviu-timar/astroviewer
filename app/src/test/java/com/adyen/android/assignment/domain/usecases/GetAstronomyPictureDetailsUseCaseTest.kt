31c8cbb7ebdd5303f0ad9fd31d15c8ed550724b2
Liviu Timar <liviu.timar@autoliv.com>
2022-07-19 15:13:33 +0300

    Added example test for Use Case

---
+++ app/src/test/java/com/adyen/android/assignment/domain/usecases/GetAstronomyPictureDetailsUseCaseTest.kt
@@ -0,0 +1,44 @@
+package com.adyen.android.assignment.domain.usecases
+
+import com.adyen.android.assignment.domain.repositories.AstronomyPictureRepository
+import com.adyen.android.assignment.utils.testPictureModelList
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.IsEqual
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mockito.anyInt
+import org.mockito.Mockito.verify
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class GetAstronomyPictureDetailsUseCaseTest {
+
+    private lateinit var useCase: GetAstronomyPictureDetailsUseCase
+    private lateinit var mockPictureRepository: AstronomyPictureRepository
+
+    @Before
+    fun setUp() {
+        mockPictureRepository = mock()
+        useCase = GetAstronomyPictureDetailsUseCase(mockPictureRepository)
+    }
+
+    // Test method naming: subjectUnderTest_input_expectedResult
+    @Test
+    fun invoke_pictureId_callsRepoWithSameId() = runTest {
+        val pictureId = 1
+
+        useCase(pictureId)
+
+        verify(mockPictureRepository).getPicture(pictureId)
+    }
+
+    @Test
+    fun invoke_anyId_returnsPicture() = runTest {
+        whenever(mockPictureRepository.getPicture(anyInt())).thenReturn(testPictureModelList[0])
+
+        val picture = useCase(anyInt())
+
+        assertThat(picture, IsEqual(testPictureModelList[0]))
+    }
+}
